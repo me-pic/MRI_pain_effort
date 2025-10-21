4d48c293ff9a9bdd8cc0241b0b2c8ffd5dc07639
Marie-Eve Picard <marie-eve.picard.2@umontreal.ca>
2025-10-21 12:36:52 -0400

    Add 3D brain visu

--- mri_pain_effort/visualization/brain_maps_viz.py
+++ mri_pain_effort/visualization/brain_maps_viz.py
@@ -68,6 +68,50 @@ def plot_brain_maps(path_data, path_output, coords_to_plot, vmax=6, extension='s
             fig.savefig(os.path.join(path_output, f'{filename}_{axis}_{str(c)}.{extension}'),
                         transparent=True, bbox_inches='tight', dpi=600)
 
+def plot_3D_brain_maps(path_data, path_output, views=['lateral', 'medial'], thresh=1, extension='svg'):
+    """
+    Plot 3D brain maps 
+    
+    Parameters
+    ----------
+    path_data: string
+        Directory containing the maps to put
+    path_output: string
+        Directory to save the output
+    views: list or string
+        Views to display
+    thresh: int
+        Value used to theshold the image
+    extension: str
+        Format in which to save the plots
+    """
+    # Make sure path_output exists
+    Path(path_output).mkdir(parents=True, exist_ok=True)
+
+    filename = os.path.basename(path_data).split('.')[0]
+
+    # Plot LH
+    plotting.plot_img_on_surf(
+        stat_map=path_data,
+        views=views,
+        surf_mesh='fsaverage5',
+        threshold=thresh,
+        hemispheres=['left'],
+        inflate=True,
+        output_file=os.path.join(path_output, f'{filename}_3D-LH.{extension}')
+    )
+    # Plot RH
+    plotting.plot_img_on_surf(
+        stat_map=path_data,
+        views=views,
+        surf_mesh='fsaverage5',
+        threshold=thresh,
+        hemispheres=['right'],
+        inflate=True,
+        output_file=os.path.join(path_output, f'{filename}_3D-RH.{extension}')
+    )
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
 
@@ -82,7 +126,7 @@ if __name__ == "__main__":
         help="Directory to save the figures"
     )
     parser.add_argument(
-        "coords",
+        "--coords",
         type=str,
         help="Dictionary containing the coordinates for each axis (x,y,z)"
     )
@@ -91,13 +135,38 @@ if __name__ == "__main__":
         type=int,
         help="Max value for the colorbar"
     )
+    parser.add_argument(
+        "--thresh",
+        type=int,
+        default=None,
+        help="Value used to theshold the image"
+    )
+    parser.add_argument(
+        "--views",
+        type=str,
+        default=None,
+        help="Views to display"
+    )
+    parser.add_argument(
+        "--plot_3D",
+        action="store_true",
+        help="If flag specified, plot image on 3D brain"
+    )
     args = parser.parse_args()
 
-    # Load coords
-    with open(args.coords, "r") as file:
-        coords = json.load(file)
-        file.close()
+    if args.plot_3D:
+        if args.views is None:
+            plot_3D_brain_maps(args.path_data, args.path_output, thresh=args.thresh)
+        else:
+            plot_3D_brain_maps(args.path_data, args.path_output, views=[args.views], thresh=args.thresh)
+    else:
+        # Load coords
+        with open(args.coords, "r") as file:
+            coords = json.load(file)
+            file.close()
 
-    plot_brain_maps(args.path_data, args.path_output, coords, args.vmax)
+        plot_brain_maps(args.path_data, args.path_output, coords, args.vmax)
+
+
 
 
